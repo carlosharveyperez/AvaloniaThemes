761809586f01c9c0f9612b9848bbb7551d019da7
Carlos <carlosharveyperez@gmail.compare>
2023-04-09 07:33:30 -0500

    More controls added to overview page.

--- ThemeViewer/ViewModels/MainWindowViewModel.cs
+++ ThemeViewer/ViewModels/MainWindowViewModel.cs
@@ -12,11 +12,12 @@ namespace ThemeViewer.ViewModels
         {
             this.WhenAnyValue(x => x.SelectedTheme)
                 .Subscribe(UpdateTheme);
+            _selectedTheme = Themes[0];
         }
 
-        public ObservableCollection<string> Themes => new() { "Dark", "Light" };
+        public ObservableCollection<string> Themes => new() { "Fluent Dark", "Fluent Light" };
 
-        private string _selectedTheme = "Dark";
+        private string _selectedTheme;
         public string SelectedTheme
         {
             get => _selectedTheme;
@@ -26,7 +27,7 @@ namespace ThemeViewer.ViewModels
         private void UpdateTheme(string theme)
         {
             if (Application.Current == null) return;
-            Application.Current.RequestedThemeVariant = theme == "Dark" ? ThemeVariant.Dark : ThemeVariant.Light;
+            Application.Current.RequestedThemeVariant = theme == Themes[0] ? ThemeVariant.Dark : ThemeVariant.Light;
         }
     }
 }
