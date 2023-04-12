a22d401277547a77f9be029d700ca72ee1ff46a2
Carlos <carlosharveyperez@gmail.compare>
2023-04-11 23:57:55 -0500

    Added support to change density.

--- ThemeViewer/ViewModels/MainWindowViewModel.cs
+++ ThemeViewer/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Avalonia;
 using Avalonia.Styling;
+using Avalonia.Themes.Fluent;
 using ReactiveUI;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace ThemeViewer.ViewModels
 {
@@ -12,6 +14,10 @@ namespace ThemeViewer.ViewModels
         {
             this.WhenAnyValue(x => x.SelectedTheme)
                 .Subscribe(UpdateTheme);
+
+            this.WhenAnyValue(x => x.IsCompact)
+                .Subscribe(UpdateDensity);
+
             _selectedTheme = Themes[0];
         }
 
@@ -24,10 +30,25 @@ namespace ThemeViewer.ViewModels
             set => this.RaiseAndSetIfChanged(ref _selectedTheme, value);
         }
 
+        private bool _isCompact = true;
+        public bool IsCompact
+        {
+            get => _isCompact;
+            set => this.RaiseAndSetIfChanged(ref _isCompact, value);
+        }
+
         private void UpdateTheme(string theme)
         {
             if (Application.Current == null) return;
             Application.Current.RequestedThemeVariant = theme == Themes[0] ? ThemeVariant.Dark : ThemeVariant.Light;
+        }
+
+        private void UpdateDensity(bool isCompact)
+        {
+            if (Application.Current == null) return;
+            var fluentTheme = Application.Current.Styles.FirstOrDefault(s => s is FluentTheme);
+            if (fluentTheme is FluentTheme ft)
+                ft.DensityStyle = IsCompact ? DensityStyle.Compact : DensityStyle.Normal;
         }
     }
 }
